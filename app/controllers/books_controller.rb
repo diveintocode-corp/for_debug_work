819274609f99fbcae08e33b023430a63aeba35cd
shounen070505 <kobayashitadao@gmail.com>
2020-08-29 10:45:15 +0900

    修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -69,6 +69,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title, :price)
+      params.require(:book).permit(:title, :price, :name)
     end
 end
